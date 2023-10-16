421b09e8246f36c1b65c56144e8149442bd28d08
pbeaudoin125 <64977819+pbeaudoin125@users.noreply.github.com>
2023-10-16 15:48:26 -0400

    Update streamlit_app.py
    test with selectbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
+
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', option)
