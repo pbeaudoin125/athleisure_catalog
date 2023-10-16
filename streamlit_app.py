57d2e0b6ca657ae6757426ffb69c001f9051085d
pbeaudoin125 <64977819+pbeaudoin125@users.noreply.github.com>
2023-10-16 16:16:22 -0400

    Update streamlit_app.py
    fetch color list from snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,14 @@ option = streamlit.selectbox(
     ('Email', 'Home phone', 'Mobile phone'))
 
 streamlit.write('You selected:', option)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select color_or_style from catalog_for_website")
+my_data_rows = my_cur.fetchall()
+my_cnx.close()
+
+option2 = streamlit.selectbox(
+    'How would you like to be contacted?', my_data_rows)
+
+streamlit.write('You selected:', option2)
